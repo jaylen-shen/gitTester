4c9a36b0a4b4f1843f5df694042b12e7828b9ae9
jiale shen <jialec836@gmail.com>
2024-06-10 21:37:28 +0800

    Init

---
+++ src/main/java/com/example/gittester/GitTesterApplication.java
@@ -0,0 +1,13 @@
+package com.example.gittester;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitTesterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitTesterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gittester/GitTesterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gittester;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitTesterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
